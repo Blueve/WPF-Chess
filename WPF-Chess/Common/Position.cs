295f29709272409f897dee6360ae8f86d290afed
Blueve <672454911@qq.com>
2015-05-25 01:05:27 +0800

    Initialize project

---
+++ WPF-Chess/Common/Position.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Chess.Common
+{
+    public sealed struct Position : IEqualityComparer<Position>
+    {
+        private Int16 _x;
+        private Int16 _y;
+
+        public Int16 X
+        {
+            get { return _x; }
+            set { _x = (value > 8 || value < 1) ? 0 : value; }
+        }
+
+        public Int16 Y
+        {
+            get { return _y; }
+            set { _y = (value > 8 || value < 1) ? 0 : value; }
+        }
+
+        public bool Equals(Position a, Position b)
+        {
+            return (a._x == b._x && a._y == b._y);
+        }
+    }
+}
---
+++ WPF-Chess/Core/Piece.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WPF_Chess.Common;
+
+namespace WPF_Chess.Core
+{
+    abstract class Piece
+    {
+        protected Position _position;
+
+        protected List<Position> _possibleMoves;
+
+        public Position Position
+        {
+            get { return _position}
+        }
+
+        /// <summary>
+        /// A cacheable property which contains all possible moves.
+        /// If the value of '_possibleMoves' is 'null', the getter will update it
+        /// by invoke virtual method 'PossibleMoves()'.
+        /// </summary>
+        public List<Position> PossibleMoves
+        {
+            get
+            {
+                return _possibleMoves ?? PossibleMoves();
+            }
+        }
+
+        /// <summary>
+        /// Move current piece to target place.
+        /// '_possibleMoves' will be set to 'null' after the position changed.
+        /// </summary>
+        /// <param name="position"></param>
+        public void MoveTo(Position position)
+        {
+            if(PossibleMoves.Contains(position))
+            {
+                _position = position;
+                _possibleMoves = null;
+            }
+            //TODO: should be throw an exception here.
+        }
+
+        /// <summary>
+        /// Return all possible moves of current piece.
+        /// </summary>
+        /// <returns></returns>
+        protected virtual List<Position> PossibleMoves();
+    }
+}

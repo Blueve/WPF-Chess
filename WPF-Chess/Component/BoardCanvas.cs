10e560afd89ed46968bc45da24ef26c17ebad9c0
Blueve <672454911@qq.com>
2015-05-26 22:48:36 +0800

    Refine methods
    Use more convenience parameter instead low-level parameter

--- WPF-Chess/Component/BoardCanvas.cs
+++ WPF-Chess/Component/BoardCanvas.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using WPF_Chess.Core;
 
 namespace WPF_Chess.Component
 {
     class BoardCanvas : Canvas
     {
+        private const int GRID_SIZE = 50;
+
         private List<Visual> _visuals = new List<Visual>();
 
         protected override int VisualChildrenCount
@@ -25,12 +29,27 @@ namespace WPF_Chess.Component
             return _visuals[index];
         }
 
-        public void AddPieces(Visual visual)
+        public void AddVisual(Visual visual)
         {
             _visuals.Add(visual);
 
             base.AddVisualChild(visual);
             base.AddLogicalChild(visual);
+        }
+
+        public void AddPiece(Piece piece)
+        {
+            DrawingVisual v = new DrawingVisual();
+            using (var dc = v.RenderOpen())
+            {
+                dc.DrawImage(
+                    piece.Image,
+                    new Rect(
+                        new Point(piece.Position.X * GRID_SIZE, piece.Position.Y * GRID_SIZE),
+                        new Size(GRID_SIZE, GRID_SIZE)
+                        ));
+            }
+            AddVisual(v);
         }
 
         public void RemoveVisual(Visual visual)

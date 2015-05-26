187182ac5d19daf6b4b82d6ca69555a1b0fb38a2
Blueve <672454911@qq.com>
2015-05-26 17:11:23 +0800

    Add test for class Core.Chessboard

---
+++ WPF-Chess-UnitTest/ChessboardTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WPF_Chess.Core;
+
+namespace WPF_Chess_UnitTest
+{
+    [TestClass]
+    public class ChessboardTest
+    {
+        Chessboard board = new Chessboard();
+
+        [TestMethod]
+        public void TestInit()
+        {
+            board.Init();
+            var pieces = board.Pieces;
+
+            Assert.AreEqual(32, pieces.Count);
+            Assert.AreEqual("(A, 8)", pieces[0].Position.ToString());
+            Assert.AreEqual("(H, 2)", pieces[31].Position.ToString());
+        }
+    }
+}

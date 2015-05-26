4571e59972c0a3d754900443e527d0c6982c9ec5
Blueve <672454911@qq.com>
2015-05-26 22:26:33 +0800

    Add BoardCanvas component

---
+++ WPF-Chess/Component/BoardCanvas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPF_Chess.Component
+{
+    class BoardCanvas : Canvas
+    {
+        private List<Visual> _visuals = new List<Visual>();
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return _visuals.Count;
+            }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return _visuals[index];
+        }
+
+        public void AddPieces(Visual visual)
+        {
+            _visuals.Add(visual);
+
+            base.AddVisualChild(visual);
+            base.AddLogicalChild(visual);
+        }
+
+        public void RemoveVisual(Visual visual)
+        {
+            _visuals.Remove(visual);
+
+            base.RemoveVisualChild(visual);
+            base.RemoveLogicalChild(visual);
+        }
+    }
+}

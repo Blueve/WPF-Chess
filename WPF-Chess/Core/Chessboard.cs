a4109183e46bb1a71cfc16b62238319e37de9ae7
Blueve <672454911@qq.com>
2015-05-26 17:12:55 +0800

    Refine naming and getter‘s type
    Use IReadOnlyList instead of IEnumerable

--- WPF-Chess/Core/Chessboard.cs
+++ WPF-Chess/Core/Chessboard.cs
@@ -15,11 +15,11 @@ namespace WPF_Chess.Core
         /// <summary>
         /// Storage all pieces
         /// </summary>
-        private readonly List<Piece> _board = new List<Piece>();
+        private readonly List<Piece> _pieces = new List<Piece>();
 
-        public IEnumerable<Piece> Board
+        public IReadOnlyList<Piece> Pieces
         {
-            get { return _board; }
+            get { return _pieces; }
         }
 
         /// <summary>
@@ -28,40 +28,40 @@ namespace WPF_Chess.Core
         public void Init()
         {
             // Clear current board
-            _board.Clear();
+            _pieces.Clear();
 
             // Create all pieces
             List<Piece> pieces = new List<Piece>()
             {
-                new Rook(S.Black, new P(1, 1)),
-                new Rook(S.Black, new P(1, 8)),
-                new Rook(S.White, new P(8, 1)),
-                new Rook(S.White, new P(8, 8)),
+                new Rook(S.Black, new P('A', 8)),
+                new Rook(S.Black, new P('H', 8)),
+                new Rook(S.White, new P('A', 1)),
+                new Rook(S.White, new P('H', 1)),
 
-                new Knight(S.Black, new P(1, 2)),
-                new Knight(S.Black, new P(1, 7)),
-                new Knight(S.White, new P(8, 2)),
-                new Knight(S.White, new P(8, 7)),
+                new Knight(S.Black, new P('B', 8)),
+                new Knight(S.Black, new P('G', 8)),
+                new Knight(S.White, new P('B', 1)),
+                new Knight(S.White, new P('G', 1)),
 
-                new Bishop(S.Black, new P(1, 3)),
-                new Bishop(S.Black, new P(1, 6)),
-                new Bishop(S.White, new P(8, 3)),
-                new Bishop(S.White, new P(8, 6)),
+                new Bishop(S.Black, new P('C', 8)),
+                new Bishop(S.Black, new P('F', 8)),
+                new Bishop(S.White, new P('C', 1)),
+                new Bishop(S.White, new P('F', 1)),
 
-                new Queen(S.Black, new P(1, 4)),
-                new Queen(S.White, new P(8, 4)),
+                new Queen(S.Black, new P('D', 8)),
+                new Queen(S.White, new P('D', 1)),
 
-                new King(S.Black, new P(1, 5)),
-                new King(S.White, new P(8, 5))
+                new King(S.Black, new P('E', 8)),
+                new King(S.White, new P('E', 1))
             };
             for (int i = 1; i <= 8; i++ )
             {
-                pieces.Add(new Pawn(S.Black, new P(2, i)));
-                pieces.Add(new Pawn(S.White, new P(7, i)));
+                pieces.Add(new Pawn(S.Black, new P(i, 2)));
+                pieces.Add(new Pawn(S.White, new P(i, 7)));
             }
 
             // Put all pieces on the board
-            _board.AddRange(pieces);
+            _pieces.AddRange(pieces);
         }
     }
 }
